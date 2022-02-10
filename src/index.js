1f8bb062ead8b0a01df0b6c3bd5eb0e4cfa7aede
cjjyyds <89148652+cjjyyds@users.noreply.github.com>
2022-02-10 00:16:33 -0800

    不改了

--- src/index.js
+++ src/index.js
@@ -28,11 +28,15 @@ let r = {
     'party_balloon',
     'party_balloons',
     'party_hat',
+    'bomb',
+    'poo',
   ],
   icon: function() {
     let {icons, stage} = r;
     let icon = icons[Math.floor(Math.random()*icons.length)]
+    let exx = Boolean(['poo', 'bomb'].includes(icon))
     icon = PIXI.Sprite.from(`../img/${icon}.png`);
+    icon.exx = exx;
     stage.addChild(icon)
     return icon;
   },
@@ -48,14 +52,15 @@ let r = {
     r.app = new PIXI.Application({
       width: r.w,
       height: r.h,
-      backgroundColor: r.bgc
+      backgroundColor: r.bgc,
+      antialias: true,
     });
     r.stage = r.app.stage;
     document.body.appendChild(r.app.view);
 
     //audio
     let context = new AudioContext();
-    let sounds = ['gan', 'ntmd', 'woc', 'bday'];
+    let sounds = ['gan', 'ntmd', 'woc', 'bday', 'exx'];
     sounds.forEach(sound => {
     
         let url = `../sound/${sound}.mp3`
@@ -116,13 +121,22 @@ let r = {
     let source = r.context.createBufferSource();
     source.buffer = r.sounds[sound];
       let gainNode = r.context.createGain()
+      let value;
       switch(sound) {
-        case 'ntmd': gainNode.gain.value = 0.5; break;
-        case 'woc': gainNode.gain.value = 0.2; break;
-        case 'gan': gainNode.gain.value = 0.5; break;
-        case 'bday': gainNode.gain.value = 1; break;
+
+        default:
+          value = 1; break;
+
+
+        case 'gan':
+          value = 0.5; break;
+
+        case 'ntmd':
+        case 'woc':
+          value = 0.2; break;
+
       }
-      
+      gainNode.gain.value = value;
       source.connect(gainNode);
       gainNode.connect(r.context.destination)
     
@@ -239,7 +253,7 @@ let r = {
           }
           break;
         case 'lose':
-          title.text = `陈姜姜又老一岁～！\n【${r.score}岁】啦！`
+          title.text = `陈姜姜又老一岁\n【${r.score}岁】啦！`
           info.text = r.mobile ? '触摸屏幕再次庆生' : '任意键再次庆生'
           title.alpha += fadingSpeed;
           info.alpha += fadingSpeed
@@ -285,6 +299,7 @@ let r = {
       let {key} = ev;
 
       action(ev);
+
         let keymap = [
           ['w', 'a', 's', 'd'],
           ['i', 'j', 'k', 'l'],
@@ -328,7 +343,7 @@ let r = {
       let key;
       let checkKey = (key) => {
         let dir = r.direction;
-        if (r.gameState) {
+        if (r.gameState == 'game') {
           switch(key) {
             case 'up': if (dir != 'down') r.direction = key; break;
             case 'left': if (dir != 'right') r.direction = key; break;
@@ -342,33 +357,34 @@ let r = {
 
       let dirs = ['↑', '←', '→', '↓']
       dirs.forEach(dir => {
-          let button = document.createElement('button');
+          let button = document.createElement('a');
           button.innerText = dir;
           document.body.append(button);
-          let base = Math.round(w / 5);
+          let base = Math.round(w / 4);
           button.style.width = base + 'px'
           button.style.height = base + 'px'
-          button.style.borderRadius = base + 'px'
+          button.style.borderRadius = Math.round(base/4) + 'px'
           button.style.fontSize = '150%';
+          button.className = 'hide';
           switch(dir) {
             case '↑':
               button.style.left = (w*0.5-base*0.5).toFixed(0) + 'px'
-              button.style.top = (h*0.75-base * 1.5).toFixed(0) + 'px'
+              button.style.top = (h-base*2.5).toFixed(0) + 'px'
               button.onclick = () => checkKey('up')
               break;
             case '↓':
               button.style.left = (w*0.5-base*0.5).toFixed(0) + 'px'
-              button.style.top = (h*0.75+base*0.5).toFixed(0) + 'px'
+              button.style.top = (h-base*1.25).toFixed(0) + 'px'
               button.onclick = () => checkKey('down')
               break;
             case '←':
-              button.style.left = (w*0.5-base*1.5).toFixed(0) + 'px'
-              button.style.top = (h*0.75-base*0.5).toFixed(0) + 'px'
+              button.style.left = (w*0.5-base*1.75).toFixed(0) + 'px'
+              button.style.top = (h-base*1.25).toFixed(0) + 'px'
               button.onclick = () => checkKey('left')
               break;
             case '→':
-              button.style.left = (w*0.5+base*0.5).toFixed(0) + 'px'
-              button.style.top = (h*0.75-base*0.5).toFixed(0) + 'px'
+              button.style.left = (w*0.5+base*0.75).toFixed(0) + 'px'
+              button.style.top = (h-base*1.25).toFixed(0) + 'px'
               button.onclick = () => checkKey('right')
           }
           
@@ -411,34 +427,188 @@ let r = {
       cjj.girdX = Math.floor(x/2)
       cjj.girdY = Math.floor(y/2)
       setPos(cjj);
+      body.clear();
+      cjj.alpha = 0;
+      delete r.direction;
     }
     
-    cjj.update = () => {
+
+    snakes.push(cjj);
+
+    let newGift = function() {
+      let gift = icon();
+      gift.anchor.set(0.5)
+      gift.width = width;
+      gift.height = height;
+      let girds = {};
+      for (let i=0; i < x; i++) {
+        girds[i] = {};
+          for (let j=0; j < y; j++) {
+            girds[i][j] = 'empty';
+          }
+      }
+      snakes.forEach(snake => {
+        let sX = snake.girdX;
+        let sY = snake.girdY;
+        if (girds[sX]) {
+          delete girds[sX][sY];
+          if (!Object.keys(girds[sX]).length) delete girds[sX];
+        }
+        
+      })
+      let rndX = Number(Object.keys(girds).randomChild);
+      let rndY = Number(Object.keys(girds[rndX]).randomChild);
+      gift.girdX = rndX
+      gift.girdY = rndY;
+      setPos(gift);
+      gift.update = () => {
+        if (gift.state == 'body') {
+          gift.alpha -= r.fadingSpeed;
+          if (gift.alpha < 0) stage.removeChild(gift);
+        }
+      }
+      snakes.push(gift);
+      return gift
+    }
+
+    let body = new PIXI.Graphics();
+
+    let score = new PIXI.Text(`陈姜姜 ${r.score} 岁啦！`, {
+      fontSize: 25,
+      fill: 0xffccff,
+      dropShadowColor: 0x000000,
+      dropShadow: true,
+      dropShadowBlur: 3,
+      dropShadowDistance: 0,
+    })
+    score.update = () => {
+      score.text = `陈姜姜 ${r.score} 岁啦！`;
+    }
+    score.x = 10;
+    score.y = 10;
+
+    let start = new PIXI.Graphics()
+    let n = new PIXI.Text(3, {
+      fontSize: 100,
+      fontWeight: 'bolder',
+      fill: 0xffffff,
+      strokeThickness: 3,
+    });
+
+    n.anchor.set(0.5)
+    // start.anchor.set(0.5)
+    n.x = w/2;
+    n.y = h/2;
+
+    start.x = w/2;
+    start.y = h/2;
+
+    start.lineStyle(1.5, 0x000000, 0.2);
+    start.beginFill(0xDE3249, 0.5);
+    let avg = Math.sqrt(width * width + height * height)
+
+    let count = 16;
+    for (let i = 0; i < count; i++) {
+      let rotation = Math.PI * i / (count/2)
+      let x = Math.sin(rotation) * avg * 2
+      let y = Math.cos(rotation) * avg * 2
+      start.drawCircle(x, y, 20)
+    }
+    start.endFill();
+    start.update = () => start.rotation += 0.005 * this.dt;
+
+    let up1 = () => {
+      if (this.gameState == 'gameStart') {
+        if (!up1.once) {
+          n.text = 3;
+          document.querySelectorAll('a').forEach(a => a.className = '')
+          up1.once = true;
+        }
+        switch(n.text) {
+          default:
+            let value = Number(n.text);
+            n.text = value - 1;
+            break;
+          case '0': 
+            r.gameState = 'game';
+            up1.once = false;
+            newGift();
+        }
+      }
+
+    }
+    up1.f = 0.75;
+    let up2 = () => {
+      
+      if (n.text === '0') {
+        n.alpha -= this.fadingSpeed * this.dt;
+        start.alpha -= this.fadingSpeed * this.dt;
+      }
+      switch(r.gameState) {
+        case 'title':
+          n.alpha = start.alpha = 0;
+          break;
+        case 'gameStart':
+          n.alpha = start.alpha = 1;
+          break;
+        case 'game':
+          n.alpha -= this.fadingSpeed * 5 * this.dt;
+          start.alpha -= this.fadingSpeed * 5 * this.dt;
+          if (n.alpha < 0) {
+            n.alpha = start.alpha = 0;
+          }
+          break;
+        case 'lose':
+          n.text = 3;
+      }
+      if (r.gameState == 'title') {
+        n.alpha = 0;
+        start.alpha = 0;
+      }
+    }
+    n.update = [up1, up2]
+    up1();up2();
+    
+    
+    let c1 = () => {
+
+      switch(r.gameState) {
+        case 'title':
+          cjj.alpha = 0;
+          break;
+        case 'gameStart':
+          break;
+        case 'game':
+          cjj.alpha += r.fadingSpeed * r.dt
+          if (cjj.alpha > 1) cjj.alpha = 1;
+        case 'lose':
+      }
+    }
+
+    let c2 = () => {
 
       let {direction, gameState} = r;
 
       // update frequency
-      cjj.update.f = (0.3) / (2 ** (r.score/30))
-
-      if (gameState == 'gameStart') {
-
-        reset();
-        snakes.forEach(snake => {
-          if (snake == cjj) return;
-          r.stage.removeChild(snake);
-        })
-        snakes = [cjj];
-        newGift();
-        r.score = 0;
-        cjj.update.f = (0.3) / (2 ** (r.score/30))
-        gameState = r.gameState = 'game';
-
         // score ratio    f(desktop) f(mobile)
         // 0     0        0.3        0.5
         // 33    1        0.15       0.25
         // 66    2        0.075      0.125
         // 100   3        0.0375     0.0625
-        
+      c2.f = (0.3) / (2 ** (r.score/30))
+
+      if (gameState == 'gameStart') {
+
+        reset();
+
+        snakes.forEach(snake => {
+          if (snake == cjj) return;
+          r.stage.removeChild(snake);
+        })
+
+        snakes = [cjj];
+        r.score = 0;
+        c2.f = (0.3) / (2 ** (r.score/30))
 
       }
       if (gameState == 'game') {
@@ -453,30 +623,38 @@ let r = {
         if (cjj.girdX == x || cjj.girdX < 0 ||
           cjj.girdY == y || cjj.girdY < 0) {
           r.gameState = 'lose';
-          reset();
+          if (cjj.girdX == x) cjj.girdX = x-1;
+          if (cjj.girdX < 0) cjj.girdX = 0;
+          if (cjj.girdY == y) cjj.girdY = y-1;
+          if (cjj.girdY < 0) cjj.girdY = 0;
+          document.querySelectorAll('a').forEach(a => a.className = 'hide')
           r.play('bday');
+          return;
         } 
         
-        if (cjj.girdX == x) cjj.girdX = x-1;
-        if (cjj.girdX < 0) cjj.girdX = 0;
-        if (cjj.girdY == y) cjj.girdY = y-1;
-        if (cjj.girdY < 0) cjj.girdY = 0;
-
-
         // draw
         body.clear();
-        snakes.forEach(snake => {
-          if (snake == snakes.firstChild) return;
-          if (snake == snakes.lastChild) return;
+        for (let i = 1; i < snakes.length - 1; i++) {
+          let snake = snakes[i];
           let {girdX, girdY} = snake;
           let x = (girdX + (r.mobile ? 0 : 1)) * width;
           let y = (girdY + (r.mobile ? 0 : 1)) * height;
-          body.beginFill(0xDE3249);
-          body.drawRect(x+2, y+2, width-4, height-4);
+          
+          let pos = snakes.length - 1 - i;
+          let alpha = 0.5 + 0.5 * (pos)/(snakes.length-2);
+
+
+          
+          body.beginFill(0xDE3249, alpha);
+          body.drawRoundedRect(x+2, y+2, width-4, height-4, 10);
           body.endFill();
+        }
+        snakes.forEach(snake => {
+
         })
-          body.lineStyle(1, 0x000000, 0.5);
-          if (!r.mobile) body.drawRoundedRect(width, height, width*(x), height*(y));
+
+        body.lineStyle(1, 0x000000, 0.5);
+        if (!r.mobile) body.drawRoundedRect(width, height, width*(x), height*(y));
           
         
 
@@ -486,7 +664,9 @@ let r = {
           last.state = 'body';
           newGift();
           r.score++
+          
           let sound = ['woc','ntmd', 'gan'].randomChild
+          if (last.exx) sound = 'exx';
           r.play(sound)
           
         }
@@ -515,67 +695,27 @@ let r = {
           }
           if (count > 1) {
             r.gameState = 'lose';
-            reset();
+            document.querySelectorAll('a').forEach(a => a.className = 'hide')
             r.play('bday');
           }
         })
 
-
-        // r.debug.text = `${checked}, ${total}/${snakes.length}, ${count}`;
-
-        
-
-
+        setPos(cjj);
 
       }
 
-      setPos(cjj);
-      
-
     }
-    cjj.update.f = 0.3;
-    
+    c2.f = 0.3;
 
-    r.cjj = cjj;
+    cjj.update = [c1, c2];
 
-
-    
-    snakes.push(cjj);
-
-    let newGift = function() {
-      let item = icon();
-      item.anchor.set(0.5)
-      item.width = width;
-      item.height = height;
-      item.girdX = between(0, x-1, true);
-      item.girdY = between(0, y-1, true);
-      setPos(item);
-      item.update = () => {
-        if (item.state == 'body') {
-          item.alpha -= r.fadingSpeed;
-          if (item.alpha < 0) stage.removeChild(item);
-        }
-      }
-      snakes.push(item);
-    }
-
-    let body = new PIXI.Graphics();
-
-    let score = new PIXI.Text(`cjj ${r.score} 岁啦！`, {
-      fontSize: 20,
-      fill: 0xff9191,
-      dropShadow: true,
-      dropShadowBlur: 3,
-      dropShadowDistance: 0,
-    })
-    score.update = () => {
-      score.text = `cjj ${r.score} 岁啦！`;
-    }
-    score.x = 10;
-    score.y = 10;
     stage.addChild(body);
     stage.addChild(cjj);
+    stage.addChild(start)
+    stage.addChild(n)
     stage.addChild(score);
+
+    
 
   }
 
